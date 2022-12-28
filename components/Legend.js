4b17f5bd642c762bf3e1ac81b4412b823acdfc3d
Lewis Mazzei <lewis@mazzei.dev>
2022-12-28 00:56:09 +0000

    make legend prettier

--- components/Legend.js
+++ components/Legend.js
@@ -1,62 +1,3 @@
-// import Grid from '@mui/material/Grid'
-// import Stack from '@mui/material/Stack'
-
-// export default function Legend({ items }) {
-//   return (
-//     <Grid
-//       container
-//       style={{
-//         'margin-top': '67vh',
-//       }}
-//     >
-//       <Grid item xs={9}></Grid>
-//       <Grid
-//         item
-//         xs={3}
-//         style={{
-//           width: '100%',
-//           marginLeft: '70%',
-//           'border-radius': '2px',
-//           border: '2px solid #ccc',
-//           'padding-top': '5px',
-//           'padding-left': '5px',
-//           'padding-right': '5px',
-//           'padding-bottom': '0',
-//         }}
-//       >
-//         {items.map((item, index) => (
-//           <Stack
-//             direction='row'
-//             key={index}
-//             style={{
-//               'align-items': 'center',
-//               'margin-bottom': '0.5em',
-//             }}
-//           >
-//             <div style={{ display: 'flex', flexDirection: 'row' }}>
-//               <div
-//                 style={{
-//                   width: '1em',
-//                   height: '1em',
-//                   border: '1px solid #ccc',
-//                   borderRadius: '2px',
-//                   backgroundColor: item.color,
-//                   marginRight: '0.5em',
-//                 }}
-//               />
-//               <span style={{ fontSize: '0.9em', fontWeight: 'bold' }}>
-//                 {item.label}
-//               </span>
-//             </div>
-//           </Stack>
-//         ))}
-//       </Grid>
-//     </Grid>
-//   )
-// }
-import Grid from '@mui/material/Grid'
-import Stack from '@mui/material/Stack'
-
 export default function Legend({ items }) {
   return (
     <div
@@ -65,16 +6,16 @@ export default function Legend({ items }) {
         width: 'fit-content',
         borderRadius: '2px',
         border: '2px solid #ccc',
-        paddingTop: '5px',
-        paddingLeft: '5px',
-        paddingRight: '5px',
+        padding: '1em 1em 0 1em',
         overflow: 'auto',
         minWidth: '125px',
-        // marginTop: '67vh',
         fontFamily: 'Helvetica',
         position: 'absolute',
         bottom: '20px',
         right: '20px',
+        borderRadius: '20px',
+        boxShadow: '0 0 10px 5px rgba(0, 0, 0, 0.1)',
+        backgroundColor: 'aliceblue',
       }}
     >
       {items.map((item, index) => (
