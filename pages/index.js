dc4c428567db11279a413c0ecf1aea8bf971e8a9
Lewis Mazzei <lewis@mazzei.dev>
2022-12-29 18:52:17 +0000

    rollback last commit

--- pages/index.js
+++ pages/index.js
@@ -17,37 +17,38 @@ import SpeciesGraph from '../components/SpeciesGraphWrapper'
 import Legend from '../components/Legend'
 import { items } from '../lib/legend'
 import Header from '../components/Header'
+import { google } from 'googleapis'
 
 export async function getServerSideProps() {
-  // const auth = new google.auth.GoogleAuth({
-  //   credentials: {
-  //     client_email: process.env.CLIENT_EMAIL,
-  //     private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-  //   },
-  //   scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-  // })
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.CLIENT_EMAIL,
+      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    },
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+  })
 
-  // const sheets = google.sheets({ version: 'v4', auth })
+  const sheets = google.sheets({ version: 'v4', auth })
 
-  // const response = await sheets.spreadsheets.values.get({
-  //   spreadsheetId: process.env.SHEET_ID,
-  //   range: 'Sheet1',
-  // })
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range: 'Sheet1',
+  })
 
-  // const sheet = response.data.values.slice(1)
-  // const allSpecies = sheet.map((row) => row[0])
+  const sheet = response.data.values.slice(1)
+  const allSpecies = sheet.map((row) => row[0])
 
-  // return {
-  //   props: {
-  //     allSpecies,
-  //     sheet,
-  //   },
-  // }
   return {
     props: {
-      string: 'Hello World',
+      allSpecies,
+      sheet,
     },
   }
+  // return {
+  //   props: {
+  //     string: 'Hello World',
+  //   },
+  // }
 }
 
 const parseImpactingString = (impactingString) => {
@@ -72,206 +73,206 @@ const parseImpactingString = (impactingString) => {
     })
 }
 
-export default function Home({ string }) {
-  console.log(string)
-  return null
-  // const [list, setList] = useState(allSpecies)
-  // const [graph, setGraph] = useState({
-  //   root: null,
-  //   data: { nodes: [], links: [] },
-  // })
-  // const [currentSpecies, setCurrentSpecies] = useState(null)
-  // const [separation, setSeparation] = useState(0)
-  // const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+export default function Home({ allSpecies, sheet }) {
+  // console.log(string)
+  // return null
+  const [list, setList] = useState(allSpecies)
+  const [graph, setGraph] = useState({
+    root: null,
+    data: { nodes: [], links: [] },
+  })
+  const [currentSpecies, setCurrentSpecies] = useState(null)
+  const [separation, setSeparation] = useState(0)
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
-  // const handleSeparationChange = (event, value) => {
-  //   setSeparation(value)
-  // }
+  const handleSeparationChange = (event, value) => {
+    setSeparation(value)
+  }
 
-  // const handleToggle = () => {
-  //   setIsDrawerOpen(!isDrawerOpen)
-  // }
+  const handleToggle = () => {
+    setIsDrawerOpen(!isDrawerOpen)
+  }
 
-  // const expandGraph = useCallback(
-  //   (graph, speciesName, maxLevel, currentLevel = 0) => {
-  //     // return the graph as is if the current recursion level exceeds the max level
-  //     if (currentLevel > maxLevel) return graph
+  const expandGraph = useCallback(
+    (graph, speciesName, maxLevel, currentLevel = 0) => {
+      // return the graph as is if the current recursion level exceeds the max level
+      if (currentLevel > maxLevel) return graph
 
-  //     // get row of impacter species
-  //     const speciesRow = allSpecies.indexOf(speciesName)
+      // get row of impacter species
+      const speciesRow = allSpecies.indexOf(speciesName)
 
-  //     // return graph as is if impacter species cannot be found
-  //     if (speciesRow === -1) return graph
+      // return graph as is if impacter species cannot be found
+      if (speciesRow === -1) return graph
 
-  //     // parse impacted species names and types
-  //     const impactedSpecies = parseImpactingString(sheet[speciesRow][14])
+      // parse impacted species names and types
+      const impactedSpecies = parseImpactingString(sheet[speciesRow][14])
 
-  //     // return graph as is if there are no impacted species
-  //     if (impactedSpecies) {
-  //       // iterate through impacted species
-  //       for (const impacted of impactedSpecies) {
-  //         // if impacted species is not in the graph
-  //         if (!graph.data.nodes.some((node) => node.id === impacted.name)) {
-  //           // add it as a node
-  //           graph.data.nodes.push({ id: impacted.name })
-  //           // and recurse to further expand graph
-  //           graph = expandGraph(
-  //             graph,
-  //             impacted.name,
-  //             maxLevel,
-  //             currentLevel + 1
-  //           )
-  //         }
+      // return graph as is if there are no impacted species
+      if (impactedSpecies) {
+        // iterate through impacted species
+        for (const impacted of impactedSpecies) {
+          // if impacted species is not in the graph
+          if (!graph.data.nodes.some((node) => node.id === impacted.name)) {
+            // add it as a node
+            graph.data.nodes.push({ id: impacted.name })
+            // and recurse to further expand graph
+            graph = expandGraph(
+              graph,
+              impacted.name,
+              maxLevel,
+              currentLevel + 1
+            )
+          }
 
-  //         // add directed link from impacter to impacted species
-  //         graph.data.links.push({
-  //           source: speciesName,
-  //           target: impacted.name,
-  //           impactTypes: impacted.impactTypes,
-  //         })
-  //       }
-  //     }
+          // add directed link from impacter to impacted species
+          graph.data.links.push({
+            source: speciesName,
+            target: impacted.name,
+            impactTypes: impacted.impactTypes,
+          })
+        }
+      }
 
-  //     for (const sheetRow of sheet) {
-  //       const impactingSpeciesName = sheetRow[0]
-  //       const impactedSpecies = parseImpactingString(sheetRow[14])
+      for (const sheetRow of sheet) {
+        const impactingSpeciesName = sheetRow[0]
+        const impactedSpecies = parseImpactingString(sheetRow[14])
 
-  //       if (!impactedSpecies) continue
+        if (!impactedSpecies) continue
 
-  //       if (impactedSpecies.some((impacted) => impacted.name === speciesName)) {
-  //         if (
-  //           !graph.data.nodes.some((node) => node.id === impactingSpeciesName)
-  //         ) {
-  //           // add it as a node
-  //           graph.data.nodes.push({ id: impactingSpeciesName })
-  //           // and recurse to further expand graph
-  //           graph = expandGraph(
-  //             graph,
-  //             impactingSpeciesName,
-  //             maxLevel,
-  //             currentLevel + 1
-  //           )
-  //         }
+        if (impactedSpecies.some((impacted) => impacted.name === speciesName)) {
+          if (
+            !graph.data.nodes.some((node) => node.id === impactingSpeciesName)
+          ) {
+            // add it as a node
+            graph.data.nodes.push({ id: impactingSpeciesName })
+            // and recurse to further expand graph
+            graph = expandGraph(
+              graph,
+              impactingSpeciesName,
+              maxLevel,
+              currentLevel + 1
+            )
+          }
 
-  //         // add directed link from impacter to impacted species
-  //         graph.data.links.push({
-  //           source: impactingSpeciesName,
-  //           target: speciesName,
-  //           impactTypes: impactedSpecies.find(
-  //             (impacted) => impacted.name === speciesName
-  //           ).impactTypes,
-  //         })
-  //       }
-  //     }
+          // add directed link from impacter to impacted species
+          graph.data.links.push({
+            source: impactingSpeciesName,
+            target: speciesName,
+            impactTypes: impactedSpecies.find(
+              (impacted) => impacted.name === speciesName
+            ).impactTypes,
+          })
+        }
+      }
 
-  //     return graph
-  //   },
-  //   [allSpecies, sheet]
-  // )
+      return graph
+    },
+    [allSpecies, sheet]
+  )
 
-  // const buildGraph = useCallback(
-  //   (speciesName, maxLevel) => {
-  //     // initialise graph with selected species
-  //     let graph = {
-  //       root: speciesName,
-  //       data: {
-  //         nodes: [{ id: speciesName }],
-  //         links: [],
-  //       },
-  //     }
-  //     // expand graph with impacted species
-  //     graph = expandGraph(graph, speciesName, maxLevel)
+  const buildGraph = useCallback(
+    (speciesName, maxLevel) => {
+      // initialise graph with selected species
+      let graph = {
+        root: speciesName,
+        data: {
+          nodes: [{ id: speciesName }],
+          links: [],
+        },
+      }
+      // expand graph with impacted species
+      graph = expandGraph(graph, speciesName, maxLevel)
 
-  //     return graph
-  //   },
-  //   [expandGraph]
-  // )
+      return graph
+    },
+    [expandGraph]
+  )
 
-  // const ListItems = ({ index, data, style }) => {
-  //   const species = data[index]
+  const ListItems = ({ index, data, style }) => {
+    const species = data[index]
 
-  //   return (
-  //     <ListItem key={index} component='div' disablePadding>
-  //       <ListItemButton
-  //         onClick={(e) => {
-  //           setCurrentSpecies(e.target.innerText)
-  //         }}
-  //         selected={species === currentSpecies}
-  //         disableTouchRipple
-  //       >
-  //         <ListItemText primary={species} />
-  //       </ListItemButton>
-  //     </ListItem>
-  //   )
-  // }
+    return (
+      <ListItem key={index} component='div' disablePadding>
+        <ListItemButton
+          onClick={(e) => {
+            setCurrentSpecies(e.target.innerText)
+          }}
+          selected={species === currentSpecies}
+          disableTouchRipple
+        >
+          <ListItemText primary={species} />
+        </ListItemButton>
+      </ListItem>
+    )
+  }
 
-  // useEffect(() => {
-  //   setGraph(buildGraph(currentSpecies, separation - 1))
-  // }, [allSpecies, buildGraph, currentSpecies, separation])
+  useEffect(() => {
+    setGraph(buildGraph(currentSpecies, separation - 1))
+  }, [allSpecies, buildGraph, currentSpecies, separation])
 
-  // return (
-  //   <div style={{ width: '100vw', height: '100vh' }}>
-  //     <Header onClick={handleToggle} isDrawerOpen={isDrawerOpen} />
-  //     <Drawer
-  //       anchor='left'
-  //       open={isDrawerOpen}
-  //       onClose={handleToggle}
-  //       variant='persistent'
-  //     >
-  //       <form>
-  //         <TextField
-  //           id='search-bar'
-  //           onInput={(e) => {
-  //             setList(() => {
-  //               if (!e.target.value) return allSpecies
-  //               return allSpecies.filter((speciesName) =>
-  //                 speciesName.toLowerCase().includes(e.target.value)
-  //               )
-  //             })
-  //           }}
-  //           variant='outlined'
-  //           size='small'
-  //           fullWidth
-  //           placeholder='Search'
-  //           sx={{ padding: '10px' }}
-  //         />
-  //       </form>
-  //       <AutoSizer>
-  //         {({ height, width }) => (
-  //           <List
-  //             height={height}
-  //             width={width}
-  //             itemData={list}
-  //             itemCount={list.length}
-  //             itemSize={46}
-  //             overscanCount={50}
-  //           >
-  //             {ListItems}
-  //           </List>
-  //         )}
-  //       </AutoSizer>
-  //     </Drawer>
-  //     {graph.root ? (
-  //       <>
-  //         <SpeciesGraph
-  //           speciesName={graph.root}
-  //           graphData={graph.data}
-  //           sheet={sheet}
-  //         />
-  //         <Legend items={items} />
-  //         <SeparationSlider maxLevel={10} onChange={handleSeparationChange} />
-  //       </>
-  //     ) : (
-  //       <>
-  //         <div></div>
-  //         <Legend items={items} />
-  //         <SeparationSlider
-  //           maxLevel={10}
-  //           onChange={handleSeparationChange}
-  //           disabled={true}
-  //         />
-  //       </>
-  //     )}
-  //   </div>
-  // )
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Header onClick={handleToggle} isDrawerOpen={isDrawerOpen} />
+      <Drawer
+        anchor='left'
+        open={isDrawerOpen}
+        onClose={handleToggle}
+        variant='persistent'
+      >
+        <form>
+          <TextField
+            id='search-bar'
+            onInput={(e) => {
+              setList(() => {
+                if (!e.target.value) return allSpecies
+                return allSpecies.filter((speciesName) =>
+                  speciesName.toLowerCase().includes(e.target.value)
+                )
+              })
+            }}
+            variant='outlined'
+            size='small'
+            fullWidth
+            placeholder='Search'
+            sx={{ padding: '10px' }}
+          />
+        </form>
+        <AutoSizer>
+          {({ height, width }) => (
+            <List
+              height={height}
+              width={width}
+              itemData={list}
+              itemCount={list.length}
+              itemSize={46}
+              overscanCount={50}
+            >
+              {ListItems}
+            </List>
+          )}
+        </AutoSizer>
+      </Drawer>
+      {graph.root ? (
+        <>
+          <SpeciesGraph
+            speciesName={graph.root}
+            graphData={graph.data}
+            sheet={sheet}
+          />
+          <Legend items={items} />
+          <SeparationSlider maxLevel={10} onChange={handleSeparationChange} />
+        </>
+      ) : (
+        <>
+          <div></div>
+          <Legend items={items} />
+          <SeparationSlider
+            maxLevel={10}
+            onChange={handleSeparationChange}
+            disabled={true}
+          />
+        </>
+      )}
+    </div>
+  )
 }
