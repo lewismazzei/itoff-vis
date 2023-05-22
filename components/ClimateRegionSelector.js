9cd2c8e8369278c9d051d21bb6aa4a935912691c
Lewis Mazzei <lewis@mazzei.dev>
2023-05-22 04:05:59 +0100

    add climate region selector

---
+++ components/ClimateRegionSelector.js
@@ -0,0 +1,93 @@
+import React from "react";
+import {
+    Radio,
+    RadioGroup,
+    FormControlLabel,
+    FormControl,
+    FormLabel,
+} from "@mui/material";
+
+export default function ClimateRegionSelector({ onChange, validRegions = [] }) {
+    return (
+        <div
+            style={{
+                position: "fixed",
+                top: "165px",
+                right: "15px",
+                width: "fit-content",
+                height: "fit-content",
+                padding: "10px 20px 10px 20px",
+                border: "2px solid #ccc",
+                textAlign: "center",
+                justifyContent: "center",
+                borderRadius: "20px",
+                boxShadow: "0 0 10px 5px rgba(0, 0, 0, 0.1)",
+                backgroundColor: "aliceblue",
+                marginTop: "10px",
+            }}
+        >
+            {console.log(validRegions)}
+            <FormControl component='fieldset'>
+                <FormLabel
+                    component='legend'
+                    style={{ marginBottom: "0.5em", letterSpacing: "0.05em" }}
+                >
+                    <strong style={{ color: "#1f2232" }}>Climate Region</strong>
+                </FormLabel>
+                <RadioGroup
+                    onChange={onChange}
+                    defaultValue={validRegions === [] ? "none" : "all"}
+                >
+                    <FormControlLabel
+                        value='all'
+                        control={<Radio disabled={validRegions === []} />}
+                        label='All'
+                    />
+                    <FormControlLabel
+                        value='polar'
+                        control={
+                            <Radio disabled={!validRegions.includes("polar")} />
+                        }
+                        label='Polar'
+                    />
+                    <FormControlLabel
+                        value='boreal'
+                        control={
+                            <Radio
+                                disabled={!validRegions.includes("boreal")}
+                            />
+                        }
+                        label='Boreal'
+                    />
+                    <FormControlLabel
+                        value='temperate'
+                        control={
+                            <Radio
+                                disabled={!validRegions.includes("temperate")}
+                            />
+                        }
+                        label='Temperate'
+                    />
+                    <FormControlLabel
+                        value='subtropical'
+                        control={
+                            <Radio
+                                disabled={!validRegions.includes("subtropical")}
+                            />
+                        }
+                        label='Subtropical'
+                    />
+                    <FormControlLabel
+                        value='tropical'
+                        control={
+                            <Radio
+                                disabled={!validRegions.includes("tropical")}
+                            />
+                        }
+                        label='Tropical'
+                    />
+                </RadioGroup>
+            </FormControl>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ import Legend from '../components/Legend'
 import { legend } from '../lib/legend'
 import Header from '../components/Header'
 import { google } from 'googleapis'
+import ClimateRegionSelector from '../components/ClimateRegionSelector'
 
 export async function getServerSideProps() {
   const auth = new google.auth.GoogleAuth({
@@ -83,195 +84,274 @@ export default function Home({ allSpecies, sheet }) {
   })
   const [currentSpecies, setCurrentSpecies] = useState(null)
   const [separation, setSeparation] = useState(1)
+  const [climateRegion, setClimateRegion] = useState('all')
   const [isDrawerOpen, setIsDrawerOpen] = useState(true)
 
   const handleSeparationChange = (event, value) => {
     setSeparation(value)
   }
 
+  const handleClimateRegionChange = (event) => {
+    setClimateRegion(event.target.value)
+  }
+
   const handleToggle = () => {
     setIsDrawerOpen(!isDrawerOpen)
   }
 
-  const expandGraph = useCallback(
-    (graph, speciesName, maxLevel, currentLevel = 0) => {
-      // return the graph as is if the current recursion level exceeds the max level
-      if (currentLevel > maxLevel) return graph
+  const getClimateRegions = useCallback(
+    (speciesRow) => {
+      if (!sheet[speciesRow]) return []
 
-      // get row of impacter species
-      const speciesRow = allSpecies.indexOf(speciesName)
-
-      // return graph as is if impacter species cannot be found
-      if (speciesRow === -1) return graph
-
-      // parse impacted species names and types
-      const impactedSpecies = parseImpactingString(sheet[speciesRow][21])
-
-      // return graph as is if there are no impacted species
-      if (impactedSpecies) {
-        // iterate through impacted species
-        for (const impacted of impactedSpecies) {
-          // if impacted species is not in the graph
-          if (!graph.data.nodes.some((node) => node.id === impacted.name)) {
-            // add it as a node
-            graph.data.nodes.push({ id: impacted.name })
-            // and recurse to further expand graph
-            graph = expandGraph(
-              graph,
-              impacted.name,
-              maxLevel,
-              currentLevel + 1
-            )
-          }
-
-          // add directed link from impacter to impacted species
-          graph.data.links.push({
-            source: speciesName,
-            target: impacted.name,
-            impactTypes: impacted.impactTypes,
-          })
-        }
+      const climateRegions = []
+      if (sheet[speciesRow][16] === '1') {
+        climateRegions.push('polar')
       }
-
-      for (const sheetRow of sheet) {
-        const impactingSpeciesName = sheetRow[0]
-        const impactedSpecies = parseImpactingString(sheetRow[21])
-
-        if (!impactedSpecies) continue
-
-        if (impactedSpecies.some((impacted) => impacted.name === speciesName)) {
-          if (
-            !graph.data.nodes.some((node) => node.id === impactingSpeciesName)
-          ) {
-            // add it as a node
-            graph.data.nodes.push({ id: impactingSpeciesName })
-            // and recurse to further expand graph
-            graph = expandGraph(
-              graph,
-              impactingSpeciesName,
-              maxLevel,
-              currentLevel + 1
-            )
-          }
-
-          // add directed link from impacter to impacted species
-          graph.data.links.push({
-            source: impactingSpeciesName,
-            target: speciesName,
-            impactTypes: impactedSpecies.find(
-              (impacted) => impacted.name === speciesName
-            ).impactTypes,
-          })
-        }
+      if (sheet[speciesRow][17] === '1') {
+        climateRegions.push('boreal')
       }
-
-      return graph
+      if (sheet[speciesRow][18] === '1') {
+        climateRegions.push('temperate')
+      }
+      if (sheet[speciesRow][19] === '1') {
+        climateRegions.push('subtropical')
+      }
+      if (sheet[speciesRow][20] === '1') {
+        climateRegions.push('tropical')
+      }
+      return climateRegions
     },
-    [allSpecies, sheet]
-  )
-
-  const buildGraph = useCallback(
-    (speciesName, maxLevel) => {
-      // initialise graph with selected species
-      let graph = {
-        root: speciesName,
-        data: {
-          nodes: [{ id: speciesName }],
-          links: [],
-        },
-      }
-      // expand graph with impacted species
-      graph = expandGraph(graph, speciesName, maxLevel)
-
-      return graph
-    },
-    [expandGraph]
-  )
-
-  const ListItems = ({ index, data, style }) => {
-    const species = data[index]
-
-    return (
-      <ListItem key={index} component='div' disablePadding style={style}>
-        <ListItemButton
-          onClick={(e) => {
-            setCurrentSpecies(e.target.innerText)
-          }}
-          selected={species === currentSpecies}
-          disableTouchRipple
-        >
-          <ListItemText primary={species} />
-        </ListItemButton>
-      </ListItem>
+    [sheet]
     )
-  }
 
-  useEffect(() => {
-    setGraph(buildGraph(currentSpecies, separation - 1))
-  }, [allSpecies, buildGraph, currentSpecies, separation])
+    const expandGraph = useCallback(
+      (graph, speciesName, maxLevel, currentLevel = 0) => {
+        // return the graph as is if the current recursion level exceeds the max level
+        if (currentLevel > maxLevel) return graph
 
-  return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Header onClick={handleToggle} isDrawerOpen={isDrawerOpen} />
-      <Drawer
-        anchor='left'
-        open={isDrawerOpen}
-        onClose={handleToggle}
-        variant='persistent'
-      >
-        <form>
-          <TextField
-            id='search-bar'
-            onInput={(e) => {
-              setList(() => {
-                if (!e.target.value) return allSpecies
-                return allSpecies.filter((speciesName) =>
-                  speciesName.toLowerCase().includes(e.target.value)
+        if (climateRegion !== 'all') {
+          const speciesRow = allSpecies.indexOf(speciesName)
+          const climateRegions = getClimateRegions(speciesRow)
+          if (!climateRegions.includes(climateRegion)) {
+            return graph
+          }
+        }
+
+        // get row of impacter species
+        const speciesRow = allSpecies.indexOf(speciesName)
+
+        // return graph as is if impacter species cannot be found
+        if (speciesRow === -1) return graph
+
+        // parse impacted species names and types
+        const impactedSpecies = parseImpactingString(sheet[speciesRow][21])
+
+        // return graph as is if there are no impacted species
+        if (impactedSpecies) {
+          // iterate through impacted species
+          for (const impacted of impactedSpecies) {
+            // if impacted species is not in the graph
+            if (
+              !graph.data.nodes.some(
+                (node) => node.id === impacted.name
                 )
-              })
-            }}
-            variant='outlined'
-            size='small'
-            fullWidth
-            placeholder='Search'
-            sx={{ padding: '10px' }}
-          />
-        </form>
-        <AutoSizer>
-          {({ height, width }) => (
-            <List
-              height={height}
-              width={width}
-              itemData={list}
-              itemCount={list.length}
-              itemSize={52}
-              overscanCount={50}
-            >
-              {ListItems}
-            </List>
-          )}
-        </AutoSizer>
-      </Drawer>
-      {graph.root ? (
-        <>
-          <SpeciesGraph
-            speciesName={graph.root}
-            graphData={graph.data}
-            sheet={sheet}
-          />
-          <Legend items={legend} />
-          <SeparationSlider maxLevel={10} onChange={handleSeparationChange} />
-        </>
-      ) : (
-        <>
-          <Legend items={legend} />
-          <SeparationSlider
-            maxLevel={10}
-            onChange={handleSeparationChange}
-            disabled={true}
-          />
-        </>
-      )}
-    </div>
-  )
-}
+                ) {
+                  if (climateRegion !== 'all') {
+                    const impactedSpeciesRow = allSpecies.indexOf(
+                      impacted.name
+                      )
+                      const climateRegions =
+                      getClimateRegions(impactedSpeciesRow)
+                      if (!climateRegions.includes(climateRegion)) {
+                        continue
+                      }
+                    }
+                    // add it as a node
+                    graph.data.nodes.push({ id: impacted.name })
+                    // and recurse to further expand graph
+                    graph = expandGraph(
+                      graph,
+                      impacted.name,
+                      maxLevel,
+                      currentLevel + 1
+                      )
+                    }
+
+                    // add directed link from impacter to impacted species
+                    graph.data.links.push({
+                      source: speciesName,
+                      target: impacted.name,
+                      impactTypes: impacted.impactTypes,
+                    })
+                  }
+                }
+
+                for (const sheetRow of sheet) {
+                  const impactingSpeciesName = sheetRow[0]
+                  const impactedSpecies = parseImpactingString(sheetRow[21])
+
+                  if (!impactedSpecies) continue
+
+                  if (
+                    impactedSpecies.some(
+                      (impacted) => impacted.name === speciesName
+                      )
+                      ) {
+                        if (
+                          !graph.data.nodes.some(
+                            (node) => node.id === impactingSpeciesName
+                            )
+                            ) {
+                              // add it as a node
+                              graph.data.nodes.push({ id: impactingSpeciesName })
+                              // and recurse to further expand graph
+                              graph = expandGraph(
+                                graph,
+                                impactingSpeciesName,
+                                maxLevel,
+                                currentLevel + 1
+                                )
+                              }
+
+                              // add directed link from impacter to impacted species
+                              graph.data.links.push({
+                                source: impactingSpeciesName,
+                                target: speciesName,
+                                impactTypes: impactedSpecies.find(
+                                  (impacted) => impacted.name === speciesName
+                                  ).impactTypes,
+                                })
+                              }
+                            }
+
+                            return graph
+                          },
+                          [allSpecies, climateRegion, getClimateRegions, sheet]
+                          )
+                          const buildGraph = useCallback(
+                            (speciesName, maxLevel) => {
+                              // ensure selected species is in the current climate region
+                              if (climateRegion !== 'all') {
+                                const climateRegions = getClimateRegions(
+                                  allSpecies.indexOf(speciesName)
+                                  )
+                                  if (!climateRegions.includes(climateRegion)) {
+                                    return { root: null, data: { nodes: [], links: [] } }
+                                  }
+                                }
+
+                                // initialise graph with selected species
+                                let graph = {
+                                  root: speciesName,
+                                  data: {
+                                    nodes: [{ id: speciesName }],
+                                    links: [],
+                                  },
+                                }
+                                // expand graph with impacted species
+                                graph = expandGraph(graph, speciesName, maxLevel)
+                                return graph
+                              },
+                              [allSpecies, climateRegion, expandGraph, getClimateRegions]
+                              )
+
+                              const ListItems = ({ index, data, style }) => {
+                                const species = data[index]
+                                return (
+                                  <ListItem key={index} component='div' disablePadding style={style}>
+                                  <ListItemButton
+                                  onClick={(e) => {
+                                    setCurrentSpecies(e.target.innerText)
+                                  }}
+                                  selected={species === currentSpecies}
+                                  disableTouchRipple
+                                  >
+                                  <ListItemText primary={species} />
+                                  </ListItemButton>
+                                  </ListItem>
+                                  )
+                                }
+
+                                useEffect(() => {
+                                  setGraph(buildGraph(currentSpecies, separation - 1))
+                                }, [allSpecies, buildGraph, currentSpecies, separation, climateRegion])
+                                return (
+                                  <div style={{ width: '100vw', height: '100vh' }}>
+                                  <Header onClick={handleToggle} isDrawerOpen={isDrawerOpen} />
+                                  <Drawer
+                                  anchor='left'
+                                  open={isDrawerOpen}
+                                  onClose={handleToggle}
+                                  variant='persistent'
+                                  >
+                                  <form>
+                                  <TextField
+                                  id='search-bar'
+                                  onInput={(e) => {
+                                    setList(() => {
+                                      if (!e.target.value) return allSpecies
+                                      return allSpecies.filter((speciesName) =>
+                                      speciesName
+                                      .toLowerCase()
+                                      .includes(e.target.value)
+                                      )
+                                    })
+                                  }}
+                                  variant='outlined'
+                                  size='small'
+                                  fullWidth
+                                  placeholder='Search'
+                                  sx={{ padding: '10px' }}
+                                  />
+                                  </form>
+                                  <AutoSizer>
+                                  {({ height, width }) => (
+                                    <List
+                                    height={height}
+                                    width={width}
+                                    itemData={list}
+                                    itemCount={list.length}
+                                    itemSize={52}
+                                    overscanCount={50}
+                                    >
+                                    {ListItems}
+                                    </List>
+                                    )}
+                                    </AutoSizer>
+                                    </Drawer>
+                                    {graph.root ? (
+                                      <>
+                                      <SpeciesGraph
+                                      speciesName={graph.root}
+                                      graphData={graph.data}
+                                      sheet={sheet}
+                                      />
+                                      <Legend items={legend} />
+                                      <SeparationSlider
+                                      maxLevel={10}
+                                      onChange={handleSeparationChange}
+                                      />
+                                      <ClimateRegionSelector
+                                      onChange={handleClimateRegionChange}
+                                      validRegions={getClimateRegions(
+                                        allSpecies.indexOf(graph.root)
+                                        )}
+                                        />
+                                        </>
+                                        ) : (
+                                          <>
+                                          <Legend items={legend} />
+                                          <SeparationSlider
+                                          maxLevel={10}
+                                          onChange={handleSeparationChange}
+                                          disabled={true}
+                                          />
+                                          <ClimateRegionSelector
+                                          onChange={handleClimateRegionChange}
+                                          validRegions={[]}
+                                          />
+                                          </>
+                                          )}
+                                          </div>
+                                          )
+                                        }
